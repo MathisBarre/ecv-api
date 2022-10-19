654b8b6a7a5302f0904628e7f4178452d03a5f37
Mathis Barré <m.barre@beapp.fr>
2022-10-19 20:54:16 +0200

    feat: send weather app

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,12 +1,96 @@
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync } from "fastify";
+import { default as axios } from "axios";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get('/', async function (request, reply) {
-    
+  fastify.get("/", async function (request, reply) {
+    const { data } = await axios.get(
+      "https://www.prevision-meteo.ch/services/json/nantes"
+    );
 
+    const hoursKey: string[] = [];
+    for (let i = 0; i <= 23; i++) {
+      hoursKey.push(i + "H00");
+    }
 
-    return { root: true }
-  })
-}
+    const daysKey: string[] = [];
+    for (let i = 0; i <= 4; i++) {
+      daysKey.push("fcst_day_" + i);
+    }
+
+    return {
+      dataProvider: "http://www.prevision-meteo.ch/",
+      currentConditions: {
+        datetime:
+          data.current_condition.date.split(".").reverse().join("-") +
+          "T" +
+          data.current_condition.hour +
+          "+02:00",
+        condition: data.current_condition.condition,
+        conditionKey: data.current_condition.condition_key,
+        temperature: {
+          value: data.current_condition.tmp,
+          unit: "°C",
+        },
+        windSpeed: {
+          value: data.current_condition.wnd_spd,
+          unit: "km/h",
+        },
+        humidity: {
+          value: data.current_condition.humidity,
+          unit: "%",
+        },
+        icon: data.current_condition.icon,
+        iconBig: data.current_condition.icon_big,
+      },
+      next5DaysConditions: daysKey.map((dayKey) => {
+        request.log.info("dayKey " + dayKey)
+        const thisDay = data[dayKey]
+        return {
+          date: thisDay.date.split(".").reverse().join("-"),
+          condition: thisDay.condition,
+          conditionKey: thisDay.condition_key,
+          icon: thisDay.icon,
+          iconBig: thisDay.icon_big,
+          temperature: {
+            min: thisDay.tmin,
+            max: thisDay.tmax,
+            unit: "°C",
+          },
+          hourly: hoursKey.map((hour) => {
+            const hourString = (hour.length === 4 ? "0" + hour : hour).replace(
+              "H",
+              ":"
+            );
+            
+            const thisHour = data[dayKey].hourly_data[hour]
+
+            return {
+              datetime:
+                data[dayKey].date.split(".").reverse().join("-") +
+                "T" +
+                hourString +
+                "+02:00",
+              condition: thisHour.CONDITION,
+              conditionKey: thisHour.CONDITION_KEY,
+              temperature: {
+                value: thisHour.TMP2m,
+                unit: "°C",
+              },
+              windSpeed: {
+                value: thisHour.WNDSPD10m,
+                unit: "km/h",
+              },
+              humidity: {
+                value: thisHour.RH2m,
+                unit: "%",
+              },
+              icon: thisHour.ICON,
+            };
+          }),
+        };
+      }),
+    };
+  });
+};
 
 export default root;
