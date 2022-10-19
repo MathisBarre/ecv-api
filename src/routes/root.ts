6fa4b2f15fa259d1c50e2af753e90458c0ba259f
Mathis Barré <m.barre@beapp.fr>
2022-10-19 12:40:57 +0200

    refacto: remove tests

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,6 +2,9 @@ import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
+    
+
+
     return { root: true }
   })
 }
