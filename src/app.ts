1caefbe80a09e4543ab38b50a32d9528de73475b
Mathis Barré <m.barre@beapp.fr>
2022-11-08 14:14:49 +0100

    feat: modify cors control

--- src/app.ts
+++ src/app.ts
@@ -18,6 +18,22 @@ const app: FastifyPluginAsync<AppOptions> = async (
     opts
 ): Promise<void> => {
   // Place here your custom code!
+  fastify.register(require('@fastify/cors'), (instance) => {
+    return (req: any, callback: any) => {
+      const corsOptions = {
+        // This is NOT recommended for production as it enables reflection exploits
+        origin: true
+      };
+  
+      // do not include CORS headers for requests from localhost
+      if (/^localhost$/m.test(req.headers.origin)) {
+        corsOptions.origin = false
+      }
+  
+      // callback expects two parameters: error and options
+      callback(null, corsOptions)
+    }
+  })
 
   // Do not touch the following lines
 
